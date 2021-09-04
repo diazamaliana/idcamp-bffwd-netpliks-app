83f0a1ef53a21cc9f4ae208be7192dd2f4e08ec8
Diaz Amaliana <diazamaliana@gmail.com>
2021-09-04 14:32:48 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css";
+import "./styles/movies.css"
+import "./components/search-bar.js";
+import "./components/tab-navbar.js";
+import "./components/hero-movie.js";
+import "./components/footer-bar.js";
+import search from "./view/search.js";
+import main from "./view/main.js";
+
+
+document.addEventListener("DOMContentLoaded", search);
+document.addEventListener("DOMContentLoaded", main);
+
+
